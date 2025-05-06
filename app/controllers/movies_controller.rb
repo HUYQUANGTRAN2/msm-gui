a067db501dff73d32871336072162535a05d36eb
loganoppenheime <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-05-06 09:21:51 +0000

    final commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,16 +42,34 @@ class MoviesController < ApplicationController
     the_id = params.fetch("path_id")
     matching_movies = Movie.where({ :id => the_id })
     the_movie = matching_movies.at(0)
-    director_id = 
+    
+    matching_directors = Director.where({ :id => the_movie.director_id })
+    the_director       = matching_directors.at(0)
 
     if the_director
-      the_director.name = params.fetch("query_name")
-      the_director.dob  = params.fetch("query_dob")
-      the_director.bio  = params.fetch("query_bio")
-      the_director.image= params.fetch("query_image")
-      the_director.save
+      the_movie.title = params.fetch("query_title")
+      the_movie.year = params.fetch("query_year")
+      the_movie.duration = params.fetch("query_duration")
+      the_movie.description = params.fetch("query_description")
+      the_movie.image = params.fetch("query_image")
+      the_movie.director_id = params.fetch("query_director_id")
+      the_movie.save
+    else 
+      the_movie.title = params.fetch("query_title")
+      the_movie.description = params.fetch("query_description")
+      the_movie.save
     end 
   
-    redirect_to("/directors/#{the_director.id}")
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
+  def delete
+    the_id = params.fetch("path_id")
+    matched_movie = Movie.where({ :id => the_id })
+    deleted_movie = matched_movie.at(0)
+    
+    deleted_movie = deleted_movie.destroy
+  
+    redirect_to("/movies")
   end
 end
